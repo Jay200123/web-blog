b2f6cd035a768bbffca3a47ac1e0ec67df22cab2
Jay200123 <kenken.ayonon@gmail.com>
2023-03-11 14:45:36 +0800

    js commit

---
+++ js/index.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+
+
+    // for redirecting to index
+
+    $("#home").on("click", function(){
+        window.location.href="index.html"
+    });
+
+    $("#profile").on("click", function(){
+        window.location.href="profile.html"
+    });
+
+    $("#contact").on("click", function(){
+        window.location.href="contact.html"
+    });
+
+   // for redirecting to other contents
+    $("#content1").on("click", function(){
+        window.location.href="content.html"
+    });
+
+    $("#content2").on("click", function(){
+        window.location.href="content2.html"
+    });
+
+    $("#content3").on("click", function(){
+        window.location.href="content3.html"
+    });
+
+    $("#content4").on("click", function(){
+        window.location.href="content4.html"
+    });
+
+    $("#content5").on("click", function(){
+        window.location.href="content5.html"
+    });
+
+    $("#content6").on("click", function(){
+        window.location.href="content6.html"
+    });
+
+    $("#content7").on("click", function(){
+        window.location.href="content7.html"
+    });
+
+    $("#content8").on("click", function(){
+        window.location.href="content8.html"
+    });
+
+    $("#content9").on("click", function(){
+        window.location.href="content9.html"
+    });
+});
