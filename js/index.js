46d2940a5e23fd25d64bfbf3e51eab93a73ce5b9
Jay200123 <kenken.ayonon@gmail.com>
2023-03-11 15:22:36 +0800

    js commit

--- js/index.js
+++ js/index.js
@@ -2,18 +2,24 @@ $(document).ready(function () {
 
 
     // for redirecting to index
-
     $("#home").on("click", function(){
         window.location.href="index.html"
     });
 
+    //redirects to profile page
     $("#profile").on("click", function(){
         window.location.href="profile.html"
     });
-
+ 
+    //redirects to contact form
     $("#contact").on("click", function(){
         window.location.href="contact.html"
     });
+
+    $("#message").on("click", function(){
+        window.location.href="contact.html"
+    });
+
 
    // for redirecting to other contents
     $("#content1").on("click", function(){
