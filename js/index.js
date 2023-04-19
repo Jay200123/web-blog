0e432ea121cb6285b5b596c23e6a85fc0f9c4019
Jay200123 <kenken.ayonon@gmail.com>
2023-04-19 22:05:21 +0800

    web-main

--- js/index.js
+++ js/index.js
@@ -5,74 +5,82 @@ $(document).ready(function () {
     $("#home").on("click", function(){
 
         setTimeout(function(){
-        window.location.href="index.html"
+        window.location.href="index.html";
         }, 500);
     });
 
     //redirects to profile page
     $("#profile").on("click", function(){
-        window.location.href="profile.html"
+        window.location.href="profile.html";
     });
  
     //redirects to contact form
     $("#contact").on("click", function(){
-        window.location.href="contact.html"
+        window.location.href="contact.html";
     });
 
     $("#message").on("click", function(){
-        window.location.href="contact.html"
+        window.location.href="contact.html";
     });
 
    // for redirecting to other contents
     $("#content1").on("click", function(){
-        window.location.href="content.html"
+        window.location.href="content.html";
     });
 
     $("#content2").on("click", function(){
-        window.location.href="content2.html"
+        window.location.href="content2.html";
     });
 
     $("#content3").on("click", function(){
-        window.location.href="content3.html"
+        window.location.href="content3.html";
     });
 
     $("#content4").on("click", function(){
-        window.location.href="content4.html"
+        window.location.href="content4.html";
     });
 
     $("#content5").on("click", function(){
-        window.location.href="content5.html"
+        window.location.href="content5.html";
     });
 
     $("#content6").on("click", function(){
-        window.location.href="content6.html"
+        window.location.href="content6.html";
     });
 
     $("#content7").on("click", function(){
-        window.location.href="content7.html"
+        window.location.href="content7.html";
     });
 
     $("#content8").on("click", function(){
-        window.location.href="content8.html"
+        window.location.href="content8.html";
     });
 
     $("#content9").on("click", function(){
-        window.location.href="content9.html"
+        window.location.href="content9.html";
     });
 
     $("#content10").on("click", function(){
-        window.location.href="content10.html"
+        window.location.href="content10.html";
     });
 
     $("#content11").on("click", function(){
-        window.location.href="content11.html"
+        window.location.href="content11.html";
     });
 
     $("#content12").on("click", function(){
-        window.location.href="content12.html"
+        window.location.href="content12.html";
     })
 
     $("#content13").on("click", function(){
-        window.location.href="content13.html"
+        window.location.href="content13.html";
+    })
+
+    $("#content14").on("click", function(){
+        window.location.href="content14.html";
+    })
+
+    $("#content15").on("click", function(){
+        window.location.href="content15.html";
     })
 });
